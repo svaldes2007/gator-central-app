7cc07f5da6aa2cfebbca57c8bba40b451eb6294c
Sasha Valdes <sashavaldes@Sashas-MacBook-Air-2.local>
2025-08-07 18:08:49 -0400

    outline

---
+++ src/InputPage.js
@@ -0,0 +1,15 @@
+import "./App.css";
+function InputPage(){
+    return(
+        <div>
+            <div className="prifile-image"></div>
+
+            <input></input>
+        </div>
+
+
+    );
+
+
+}
+export default InputPage;
